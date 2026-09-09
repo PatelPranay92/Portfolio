6d7dc986d9dbcfb77f2bfc1ffcdb12cbd24857b0
PatelPranay92 <patelpranay2004@gmail.com>
2026-09-09 09:39:53 +0530

    feat: implement MongoDB connection utility with caching for Mongoose

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
-}
-
 // @ts-ignore
 let cached = global.mongoose;
 
@@ -15,6 +11,10 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable');
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
